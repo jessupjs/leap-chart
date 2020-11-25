e9bbf1dd4fd83bb3f26b97172595247c3a939de6
Jared Jessup <jessupjs@gmail.com>
2020-11-24 19:15:54 -0500

    Init commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index/index.component';
+import { HeaderComponent } from './mods/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Leap Chart
+
+Stategies for evaluating user interactions with different charts and with different controls.
+
+#### Setup
+1. Make sure to `cd client` and`npm i`.
+2. Run in dev-mode with `ng serve --port=4321`.
